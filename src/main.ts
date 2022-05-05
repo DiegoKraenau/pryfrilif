a2273f01d04a6faf89ec4cf2b480b700a9db12e9
diegokraenau <diegokraenau@gmail.com>
2022-05-04 22:17:09 -0500

    asfasf

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     }),
   );
   await app.listen(process.env.PORT || 3000);
-  console.log('aedddddfffa');
+  console.log('aeddddasfasfdfffa');
 
   //Debug
   logger.log(`Server rendering in ${await app.getUrl()}`);
