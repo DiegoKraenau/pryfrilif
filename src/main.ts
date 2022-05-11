723dbc90014d69e4f2f8a7565784826a56c56699
diegokraenau <diegokraenau@gmail.com>
2022-05-10 22:41:52 -0500

    Adding new data

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,6 @@ async function bootstrap() {
     }),
   );
   await app.listen(process.env.PORT || 3000);
-  console.log('aeddddasfasfdfasfasfffa');
-
   //Debug
   logger.log(`Server rendering in ${await app.getUrl()}`);
   logger.log(`Environment: ${process.env.DATABASE_HOST}`);
